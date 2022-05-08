22b578a9a9cb27d9f733e777fa25000160e77619
Demyankov home_PC <demyantaurus@gmail.ru>
2022-05-08 14:12:27 +0300

    Trello v.1.132 mobile

--- app.e87ca0bd.js
+++ app.e87ca0bd.js
@@ -794,7 +794,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60900" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61768" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -971,4 +971,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","app/app.js"], null)
-//# sourceMappingURL=/app.e87ca0bd.js.map
+//# sourceMappingURL=app.e87ca0bd.js.map
