c8892f2823475174eb08d1ccca88204a1d978cec
Demyankov home_PC <demyantaurus@gmail.ru>
2022-05-08 13:22:58 +0300

    Trello v.1.87 mobile

--- style.251b6741.js
+++ style.251b6741.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\image\\arrow.svg":[["arrow.23d0505e.svg","image/arrow.svg"],"image/arrow.svg"],"./..\\image\\arrow_hover.svg":[["arrow_hover.f8fdbd65.svg","image/arrow_hover.svg"],"image/arrow_hover.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\image\\arrow.svg":[["arrow.23d0505e.svg","image/arrow.svg"],"image/arrow.svg"],"./..\\image\\arrow_hover.svg":[["arrow_hover.f8fdbd65.svg","image/arrow_hover.svg"],"image/arrow_hover.svg"],"./..\\image\\icons.png":[["icons.bc693cbb.png","image/icons.png"],"image/icons.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58892" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59315" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
