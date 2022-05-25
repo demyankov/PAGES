d7efe202f6f6d9bfc57d1bd5cbcb6d8755a3d6ca
Demyankov home_PC <demyantaurus@gmail.ru>
2022-05-26 01:46:39 +0300

    Black Room_Booking

--- js/booking.js
+++ js/booking.js
@@ -1,116 +1,137 @@
-
 // РАБОТА С ФОРМОЙ БРОНИРОВАНИЯ
 
-let bookingWrapper = document.querySelector('.booking')
-let bookingCardwrapper = document.querySelector('.booking-card-wrapper')
-let bookingCloseCard = document.querySelector('#booking-card__close')
-let bookingCancelCard = document.querySelector('#booking-close_btn')
+let bookingWrapper = document.querySelector(".booking");
+let bookingCardwrapper = document.querySelector(".booking-card-wrapper");
+let bookingCloseCard = document.querySelector("#booking-card__close");
+let bookingCancelCard = document.querySelector("#booking-close_btn");
 
-bookingWrapper.addEventListener('click', (e)=>{
-    let el = e.target.closest('.booking__times-item')  
-   if (el && !el.className.includes('booked')) {
-    bookingCardwrapper.classList.toggle('booking-active')
-    html.style.overflow = 'hidden'
-   } else{
-       html.style.overflow = 'auto'
-}
-}
-)
+bookingWrapper.addEventListener("click", (e) => {
+  let el = e.target.closest(".booking__times-item");
+  if (el && !el.className.includes("booked")) {
+    bookingCardwrapper.classList.toggle("booking-active");
+    html.style.overflow = "hidden";
+  } else {
+    html.style.overflow = "auto";
+  }
+});
 
-bookingCloseCard.addEventListener('click', ()=>{
-    html.style.overflow = 'auto'
-    bookingCardwrapper.classList.remove('booking-active')
-    
-})
+bookingCloseCard.addEventListener("click", () => {
+  html.style.overflow = "auto";
+  bookingCardwrapper.classList.remove("booking-active");
+});
 
-bookingCancelCard.addEventListener('click', ()=>{
-    html.style.overflow = 'auto'
-    bookingCardwrapper.classList.remove('booking-active')
-})
+bookingCancelCard.addEventListener("click", () => {
+  html.style.overflow = "auto";
+  bookingCardwrapper.classList.remove("booking-active");
+});
 
+let costGame = document.querySelector("#cost-game");
+let modeSelect = document.querySelector(".mode-select");
 
-let costGame = document.querySelector('#cost-game')
-let modeSelect = document.querySelector('.mode-select')
+modeSelect.addEventListener("change", () => {
+  costGame.innerText = modeSelect.value;
+});
 
-modeSelect.addEventListener('change',()=>{
-    costGame.innerText = modeSelect.value
-})
+// РАБОТА С ОТОБРАЖЕНИЕМ РАСПИСАНИЯ
+let btnPrevious = document.querySelector("#btn_previous");
+let btnNext = document.querySelector("#btn_next");
+let bookingForm = document.querySelector("#booking-form");
+let bookingDay = bookingForm.querySelectorAll(".booking__day");
+let bookingTimes = bookingForm.querySelectorAll(".booking__times");
+let countBookingDay = bookingDay.length;
+let lastOpenBookingDay = countBookingDay;
+let firstOpenBookingDay = 0;
 
-// РАБОТА С ОТОБРАЖЕНИЕМ РАСПИСАНИЯ 
-let btnPrevious = document.querySelector('#btn_previous')
-let btnNext = document.querySelector('#btn_next')
-let bookingForm = document.querySelector('#booking-form')
-let bookingDay = bookingForm.querySelectorAll('.booking__day')
-let bookingTimes = bookingForm.querySelectorAll('.booking__times')
-let countBookingDay = bookingDay.length
-let lastOpenBookingDay = countBookingDay
-let firstOpenBookingDay = 0
-
-if (bookingDay.length>7) { 
-    for (let i=7; i<bookingDay.length;i++) {
-        bookingDisplayNone (i)
-    }    
-    lastOpenBookingDay = 6
+if (bookingDay.length > 7) {
+  for (let i = 7; i < bookingDay.length; i++) {
+    bookingDisplayNone(i);
+  }
+  lastOpenBookingDay = 6;
 }
 
-btnNext.addEventListener('click',()=>{
-    if (bookingDay.length>7 && lastOpenBookingDay!==countBookingDay-1) {
-        for (let i=0; i<=lastOpenBookingDay;i++) {
-            bookingDisplayNone (i)
-        }
-        
-        let startOpen = lastOpenBookingDay+1
-        let endOpen = lastOpenBookingDay+8
-       
-        for (let i = startOpen; i<endOpen;i++) {
-            if (i<countBookingDay){  
-                firstOpenBookingDay++
-                lastOpenBookingDay++
-                bookingDisplayFlex (i)
-            }
-    }}
-})
-
-btnPrevious.addEventListener('click',()=>{
-    if (lastOpenBookingDay>=7) {
-        for (let i = firstOpenBookingDay; i<=lastOpenBookingDay;i++) {
-            bookingDisplayNone (i)
-          }
-    
-    let startOpen = firstOpenBookingDay-7
-    let endOpen = lastOpenBookingDay-7
-
-    console.log(startOpen)
-    console.log(endOpen)
-    if (startOpen>=0){
-        for (let i = startOpen; i<=endOpen;i++) {
-            firstOpenBookingDay--
-            lastOpenBookingDay--
-            bookingDisplayFlex (i)
-        } 
+btnNext.addEventListener("click", () => {
+  if (bookingDay.length > 7 && lastOpenBookingDay !== countBookingDay - 1) {
+    for (let i = 0; i <= lastOpenBookingDay; i++) {
+      bookingDisplayNone(i);
     }
-       
-    if (startOpen<0){
-        firstOpenBookingDay = 0
-        lastOpenBookingDay = 6
 
-        for (let i = 0; i<=6;i++) {
-            bookingDisplayFlex (i)
-        }  
-    }
-    }
-})
+    let startOpen = lastOpenBookingDay + 1;
+    let endOpen = lastOpenBookingDay + 8;
 
-function bookingDisplayNone (i) {
-    bookingDay[i].style.display = 'none'
-    bookingTimes[i].style.display = 'none'
+    for (let i = startOpen; i < endOpen; i++) {
+      if (i < countBookingDay) {
+        firstOpenBookingDay++;
+        lastOpenBookingDay++;
+        bookingDisplayFlex(i);
+      }
+    }
+  }
+});
+
+btnPrevious.addEventListener("click", () => {
+  if (lastOpenBookingDay >= 7) {
+    for (let i = firstOpenBookingDay; i <= lastOpenBookingDay; i++) {
+      bookingDisplayNone(i);
+    }
+
+    let startOpen = firstOpenBookingDay - 7;
+    let endOpen = lastOpenBookingDay - 7;
+
+    console.log(startOpen);
+    console.log(endOpen);
+    if (startOpen >= 0) {
+      for (let i = startOpen; i <= endOpen; i++) {
+        firstOpenBookingDay--;
+        lastOpenBookingDay--;
+        bookingDisplayFlex(i);
+      }
+    }
+
+    if (startOpen < 0) {
+      firstOpenBookingDay = 0;
+      lastOpenBookingDay = 6;
+
+      for (let i = 0; i <= 6; i++) {
+        bookingDisplayFlex(i);
+      }
+    }
+  }
+});
+
+function bookingDisplayNone(i) {
+  bookingDay[i].style.display = "none";
+  bookingTimes[i].style.display = "none";
 }
 
-function bookingDisplayFlex (i) {
-    bookingDay[i].style.display = 'flex'
-    bookingTimes[i].style.display = 'flex'
+function bookingDisplayFlex(i) {
+  bookingDay[i].style.display = "flex";
+  bookingTimes[i].style.display = "flex";
 }
 
+//ЗАБРОНИРОВАТЬ
 
+let btnBooked = document.querySelector("#booking-submit_btn");
+let inputConfidece = document.querySelector("#booking-card__confidence");
+let inputPhone = document.querySelector("#phone");
 
+inputConfidece.addEventListener("input", () => {
+  activateBtnSubmit();
+});
 
+inputPhone.addEventListener("input", () => {
+  activateBtnSubmit();
+});
+
+function activateBtnSubmit() {
+  console.log(inputConfidece.checked);
+  console.log(inputPhone.value);
+  if (inputConfidece.checked && inputPhone.value) {
+    btnBooked.classList.remove("submit_btn");
+    btnBooked.classList.add("submit_btn-active");
+    btnBooked.disabled = false;
+  } else {
+    btnBooked.classList.add("submit_btn");
+    btnBooked.classList.remove("submit_btn-active");
+    btnBooked.disabled = true;
+  }
+}
